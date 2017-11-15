74d5aeb1355722a2f8ad05e891e5cd034d55c7d3
pwahlbom <philip@wahlbom.net>
2017-11-15 11:39:06 -0500

    11/15 Morning work

--- CFPortfolio01/Controllers/HomeController.cs
+++ CFPortfolio01/Controllers/HomeController.cs
@@ -8,22 +8,52 @@ namespace CFPortfolio01.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Start()
         {
+            ViewBag.Title = "START";
+            ViewBag.Message = "Your Start page.";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Title = "ABOUT";
+            ViewBag.Message = "Your About page.";
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult JSExercises()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Title = "JSEXERCISES";
+            ViewBag.Message = "Your JSExercises page.";
+            return View();
+        }
 
+        public ActionResult Portfolio()
+        {
+            ViewBag.Title = "PORTFOLIO";
+            ViewBag.Message = "Your Portfolio page.";
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            ViewBag.Title = "RESUME";
+            ViewBag.Message = "Your Resume page.";
+            return View();
+        }
+
+        public ActionResult Other()
+        {
+            ViewBag.Title = "OTHER";
+            ViewBag.Message = "Your Other page.";
+            return View();
+        }
+
+    public ActionResult Contact()
+        {
+            ViewBag.Title = "CONTACT";
+            ViewBag.Message = "Your Contact page.";
             return View();
         }
     }
